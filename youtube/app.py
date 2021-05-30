80ce5d7cf8d57977fb330269a892205e77b67a87
amir1m <amir_mukeri@yahoo.com>
2021-05-30 18:41:49 +0530

    ajax

--- youtube/app.py
+++ youtube/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, URL
@@ -11,13 +11,22 @@ class InputForm(FlaskForm):
     yturl = StringField(label=('Enter YouTube Video URL:'), validators=[DataRequired(),URL()])
     submit = SubmitField(label=('Submit'))
 
-    @app.route('/', methods=('GET', 'POST'))
-    def index():
-        form = InputForm()
-        if form.validate_on_submit():
-            text = get_video_text(form.yturl.data)
-            summary = create_summary(text)
-            s_summary = create_summary(text)
-            return f'''<h1> First summary</h1>{summary}
-                            <p><h2>Second summary</h2>{s_summary}'''
-        return render_template('index.html', form=form)
+@app.route('/')
+def home():
+    form = InputForm()
+    if request.method == 'POST' and form.validate_on_submit():
+        return redirect(url_for('summarize'))
+
+@app.route('/summarize/', methods=(['GET', 'POST']))
+def summarize():
+    # form = InputForm()
+    # if form.validate_on_submit():
+        # text = get_video_text(form.yturl.data)
+        # summary = create_summary(text)
+        # s_summary = create_summary(text)
+        # return jsonify(data={'first_summary':summary, 'second_summary':s_summary})
+    return jsonify(data={'first_summary':"summry1", 'second_summary':"summary2"})
+    return jsonify(data=form.errors)
+
+if __name__ == '__main__':
+    app.run(debug=True)
